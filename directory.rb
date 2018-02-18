16e7a04edcb10cbef3c07c1b54c60d7c67d1b127
Ammar Arjomand <persianCookBook@gmail.com>
2018-02-18 19:37:49 +0000

    added file saving functionality

--- directory.rb
+++ directory.rb
@@ -1,35 +1,95 @@
+@students = [] # an empty array accessible to all methods
+
 def print_header
-  puts "The students of Northerners Academy"
-  puts "-------------"
+  puts "The students of Northerners Academy".center(50)
+  puts "-------------".center(50)
 end
-def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+
+
+def print_students_list
+ @students.each.with_index(1) do |student, index|
+    #if student[:name][0] == "D"
+    #if student[:name].length < 12
+      puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
+    #end
   end
+  #count = 0
+  #while count <@students.length
+  #  student =@students[count]
+  #  puts "#{count + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
+  #  count += 1
+  #end
 end
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+
+
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
+
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
   #create an empty array
-  students =[]
+  # No longer needed - students =[]
   #get the first name
   name = gets.chomp
   #while the name is not empty, repeat this code
   while !name.empty? do
     #add the student hash to the array
-    students << {name: name, cohort: :november}
-    puts "Now we have #{students.count} students"
+   @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
     name = gets.chomp
   end
-  #return the array of students
-  students
 end
+
+def save_students
+  # open the file for writing
+  file = File.open("students.csv", "w")
+  # iterate over the array of students
+  @students.each do |student|
+    student_data = [student[:name], student[:cohort]]
+    csv_line = student_data.join(",")
+    file.puts csv_line
+  end
+  file.close
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" # 9 because we'll be adding more items
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "3"
+      save_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
+  end
+end
+
 #nothing happens until we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
